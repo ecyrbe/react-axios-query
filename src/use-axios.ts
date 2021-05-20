eaee2bc8a215b75260b452ca34b0c994e0ff457f
ecyrbe <ecyrbe@gmail.com>
2021-05-20 23:51:51 +0200

    feat: first implementation

---
+++ src/use-axios.ts
@@ -0,0 +1,8 @@
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
+
+export const useAxios = <T>(config: AxiosRequestConfig) =>
+  useQuery<AxiosResponse<T>, AxiosError<T>>(
+    [config.url, config.params].filter(Boolean),
+    async () => await axios.request<T>(config)
+  );
