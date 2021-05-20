8f3713ecf2ec764cc9a8451ac964108af86e833d
ecyrbe <ecyrbe@gmail.com>
2021-05-21 00:24:37 +0200

    fix: no axait needed

--- src/use-axios.ts
+++ src/use-axios.ts
@@ -2,7 +2,6 @@ import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { useQuery } from 'react-query';
 
 export const useAxios = <T>(config: AxiosRequestConfig) =>
-  useQuery<AxiosResponse<T>, AxiosError<T>>(
-    [config.url, config.params].filter(Boolean),
-    async () => await axios.request<T>(config)
+  useQuery<AxiosResponse<T>, AxiosError<T>>([config.url, config.params].filter(Boolean), () =>
+    axios.request<T>(config)
   );
